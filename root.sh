1260acbb820b1d9551a3acc2b7be03943ab2c9d7
Orion Hridoy <hridoyorion@gmail.com>
2023-08-13 21:52:26 -0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -7,12 +7,10 @@ export ALLOW_PASSWORD_CHANGE=1
 # List all users and set random strong passwords
 ls -1 /var/cpanel/users | while read user; do
 pass=`strings /dev/urandom | tr -dc .~?_A-Z-a-z-0-9 | head -c16 | xargs`
-echo "$user $pass" >> hexadecimal.txt
+echo "$user $pass" >> karma-user.txt
 
 # Change the password & update FTP login database
 /scripts/ftpupdate
 /scripts/realchpass $user $pass
 
 done
-echo  -e "\n\n==============[ RESULTS ]===============\n\n"
-cat hexadecimal.txt
