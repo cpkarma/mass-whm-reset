deb8bf6e9a4e2f4c9ea79cbe333027760dd82995
Orion Hridoy <hridoyorion@gmail.com>
2023-08-13 21:53:36 -0700

    Create karma.sh

---
+++ karma.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Avoid cPanel warnings
+ALLOW_PASSWORD_CHANGE=1
+export ALLOW_PASSWORD_CHANGE=1
+
+# List all users and set random strong passwords
+ls -1 /var/cpanel/users | while read user; do
+pass=`strings /dev/urandom | tr -dc .~?_A-Z-a-z-0-9 | head -c16 | xargs`
+echo "$pass" >> karma-user.txt
+
+# Change the password & update FTP login database
+/scripts/ftpupdate
+/scripts/realchpass $user $pass
+
+done
