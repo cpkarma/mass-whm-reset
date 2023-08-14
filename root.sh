5d0661efb48b26a32a94d6cb8760933232be0441
Orion Hridoy <hridoyorion@gmail.com>
2023-08-13 21:50:18 -0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -7,7 +7,7 @@ export ALLOW_PASSWORD_CHANGE=1
 # List all users and set random strong passwords
 ls -1 /var/cpanel/users | while read user; do
 pass=`strings /dev/urandom | tr -dc .~?_A-Z-a-z-0-9 | head -c16 | xargs`
-echo "$user $pass" >> hexadecimal.txt
+echo "$pass" >> hexadecimal.txt
 
 # Change the password & update FTP login database
 /scripts/ftpupdate
